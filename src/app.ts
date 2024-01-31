e02a8c58cfdbc78a609bc81709de900257cb7295
Jhoset NG <adm_dng@hotmail.com>
2024-01-31 17:06:27 -0500

    Integrate Github-sha256-Middleware :)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware';
 
 
 (() => {
@@ -12,6 +13,7 @@ function main() {
     const app = express();
     const controller = new GithubController();
     app.use(express.json())
+    app.use(GithubSha256Middleware.verifySignature);
     app.post('/api/github', controller.webhookHandler)
 
 
